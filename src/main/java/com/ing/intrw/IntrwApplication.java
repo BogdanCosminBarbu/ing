2e8853b4221c7ccaadc0948097d7531d157cf5bc
BogdanCosminBarbu <barbubogdan1337@gmail.com>
2024-12-09 11:10:41 +0200

    First commit

---
+++ src/main/java/com/ing/intrw/IntrwApplication.java
@@ -0,0 +1,13 @@
+package com.ing.intrw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/intrw/IntrwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.intrw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
