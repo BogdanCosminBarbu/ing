25ab62d659ce0aaf61c40f0622bdc2e5cb8c852f
BogdanCosminBarbu <barbubogdan1337@gmail.com>
2024-12-09 11:30:44 +0200

    added repository

---
+++ src/main/java/com/ing/intrw/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ing.intrw.repository;
+
+import com.ing.intrw.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
