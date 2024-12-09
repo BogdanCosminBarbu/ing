0a0681e0c326804ed71811b3eef330bb2cd80492
BogdanCosminBarbu <barbubogdan1337@gmail.com>
2024-12-09 11:27:56 +0200

    Added the main class

---
+++ src/main/java/com/ing/intrw/model/Product.java
@@ -0,0 +1,28 @@
+package com.ing.intrw.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String category;
+    private String sku;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
